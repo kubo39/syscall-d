d07e3aeeb28b0b91188a36a7369ca0e174847757
kubo39 <kubo39@gmail.com>
2016-12-26 14:53:04 +0900

    Cleanup

--- source/syscalld/package.d
+++ source/syscalld/package.d
@@ -4,15 +4,15 @@ module syscalld;
 
 version(D_InlineAsm_X86_64)
 {
-    version(linux) import syscalld.arch.linux_x86_64;
-    else version(OSX) import syscalld.arch.osx_x86_64;
+    version(linux) import syscalld.os.linux_x86_64;
+    else version(OSX) import syscalld.os.osx_x86_64;
     else static assert(false, "Not supoorted your platform/architecuture.");
 
     import syscalld.arch.syscall_x86_64;
 }
 else version(D_InlineAsm_X86)
 {
-    version(linux) import syscalld.arch.linux_x86;
+    version(linux) import syscalld.os.linux_x86;
     else static assert(false, "Not supoorted your platform/architecuture.");
     import syscalld.arch.syscall_x86;
 }
