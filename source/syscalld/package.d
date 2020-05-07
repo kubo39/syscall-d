75384ea2df574620fd8b19a4b503ee7388f7648e
John Colvin <john.loughran.colvin@gmail.com>
2020-05-07 19:09:56 +0100

    import static assert messages

--- source/syscalld/package.d
+++ source/syscalld/package.d
@@ -7,17 +7,17 @@ version(D_InlineAsm_X86_64)
     version(linux) import syscalld.os.linux_x86_64;
     else version(OSX) import syscalld.os.osx_x86_64;
     else version(FreeBSD) import syscalld.os.freebsd_x86_64;
-    else static assert(false, "Not supoorted your platform/architecuture.");
+    else static assert(false, "Not supported on your platform/architecuture.");
 
     import syscalld.arch.syscall_x86_64;
 }
 else version(D_InlineAsm_X86)
 {
     version(linux) import syscalld.os.linux_x86;
-    else static assert(false, "Not supoorted your platform/architecuture.");
+    else static assert(false, "Not supported on your platform/architecuture.");
     import syscalld.arch.syscall_x86;
 }
-else static assert(false, "Not supoorted your platform/architecuture.");
+else static assert(false, "Not supported on your platform/architecuture.");
 
 unittest
 {
