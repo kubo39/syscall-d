0387efa3b73905da4283d99f0cebc7e7b09909dc
Martin Kinkelin <mkinkelin@symmetryinvestments.com>
2021-12-14 20:28:51 +0100

    *Publicly* import symbols in syscalld.package
    This fixes the example in `examples`.

--- source/syscalld/package.d
+++ source/syscalld/package.d
@@ -4,18 +4,18 @@ module syscalld;
 
 version(D_InlineAsm_X86_64)
 {
-    version(linux) import syscalld.os.linux_x86_64;
-    else version(OSX) import syscalld.os.osx_x86_64;
-    else version(FreeBSD) import syscalld.os.freebsd_x86_64;
+    version(linux) public import syscalld.os.linux_x86_64;
+    else version(OSX) public import syscalld.os.osx_x86_64;
+    else version(FreeBSD) public import syscalld.os.freebsd_x86_64;
     else static assert(false, "Not supported on your platform/architecuture.");
 
-    import syscalld.arch.syscall_x86_64;
+    public import syscalld.arch.syscall_x86_64;
 }
 else version(D_InlineAsm_X86)
 {
-    version(linux) import syscalld.os.linux_x86;
+    version(linux) public import syscalld.os.linux_x86;
     else static assert(false, "Not supported on your platform/architecuture.");
-    import syscalld.arch.syscall_x86;
+    public import syscalld.arch.syscall_x86;
 }
 else static assert(false, "Not supported on your platform/architecuture.");
 
